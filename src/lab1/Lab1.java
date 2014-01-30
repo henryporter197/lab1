2a667ce63dc064ba282fd27a6616022aeb914429
Porter <henry.porter197@BT2372-11.local>
2014-01-29 20:52:51 -0800

    initialcommit

---
+++ src/lab1/Lab1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lab1;
+import javax.swing.*;
+
+/**
+ *
+ * @author henry.porter197
+ */
+public class Lab1 extends JFrame
+{
+    public static final int windowSize = 400;
+    
+    public Lab1()
+    {
+        super();
+        setSize(windowSize,windowSize);
+        setTitle("Swing Test");
+        
+    }
+    
+}
